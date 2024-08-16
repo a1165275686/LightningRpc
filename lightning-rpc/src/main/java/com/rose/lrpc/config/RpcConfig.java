b763ee601100b2998b497752f1f0033b8d7a09a3
yuan <1165275686@qq.com>
2024-08-16 17:47:27 +0800

    为部分代码部分注释

--- lightning-rpc/src/main/java/com/rose/lrpc/config/RpcConfig.java
+++ lightning-rpc/src/main/java/com/rose/lrpc/config/RpcConfig.java
@@ -5,12 +5,23 @@ import lombok.Data;
 @Data
 public class RpcConfig {
 
+    /*
+     * 服务名称
+     */
     private String name = "lightning-rpc";
 
+    /*
+     * 服务版本
+     */
     private String version = "1.0";
-
+    /*
+     * 服务端地址
+     */
     private String serverHost = "localhost";
 
+    /*
+     * 服务端端口
+     */
     private Integer serverPort = 8080;
 
 }
