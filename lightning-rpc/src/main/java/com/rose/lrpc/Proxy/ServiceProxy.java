1ea378e387fc9469f23b1bd8dae8017b26367024
yuan <1165275686@qq.com>
2024-08-21 17:53:39 +0800

    优化客户端调用代码

--- lightning-rpc/src/main/java/com/rose/lrpc/Proxy/ServiceProxy.java
+++ lightning-rpc/src/main/java/com/rose/lrpc/Proxy/ServiceProxy.java
@@ -1,31 +1,26 @@
 package com.rose.lrpc.Proxy;
 
 import cn.hutool.core.collection.CollUtil;
-import cn.hutool.core.util.IdUtil;
+import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpResponse;
 import com.rose.lrpc.RpcApplication;
 import com.rose.lrpc.config.RpcConfig;
 import com.rose.lrpc.constant.RpcConstant;
 import com.rose.lrpc.model.RpcRequest;
 import com.rose.lrpc.model.RpcResponse;
 import com.rose.lrpc.model.ServiceMetaInfo;
-import com.rose.lrpc.protocol.*;
 import com.rose.lrpc.registry.Registry;
 import com.rose.lrpc.registry.RegistryFactory;
 import com.rose.lrpc.serializer.Serializer;
 import com.rose.lrpc.serializer.SerializerFactory;
-import io.vertx.core.Vertx;
-import io.vertx.core.buffer.Buffer;
-import io.vertx.core.net.NetClient;
+import com.rose.lrpc.server.tcp.VertxTcpClient;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
 
 public class ServiceProxy implements InvocationHandler {
-
-
     /**
      * 调用代理
      *
@@ -46,8 +41,6 @@ public class ServiceProxy implements InvocationHandler {
                 .args(args)
                 .build();
         try {
-            // 序列化
-            byte[] bodyBytes = serializer.serialize(rpcRequest);
             // 从注册中心获取服务提供者请求地址
             RpcConfig rpcConfig = RpcApplication.getRpcConfig();
             Registry registry = RegistryFactory.getInstance(rpcConfig.getRegistryConfig().getRegistry());
@@ -60,55 +53,31 @@ public class ServiceProxy implements InvocationHandler {
             }
             ServiceMetaInfo selectedServiceMetaInfo = serviceMetaInfoList.get(0);
             // 发送 TCP 请求
-            Vertx vertx = Vertx.vertx();
-            NetClient netClient = vertx.createNetClient();
-            CompletableFuture<RpcResponse> responseFuture = new CompletableFuture<>();
-            netClient.connect(selectedServiceMetaInfo.getServicePort(), selectedServiceMetaInfo.getServiceHost(),
-                    result -> {
-                        if (result.succeeded()) {
-                            System.out.println("Connected to TCP server");
-                            io.vertx.core.net.NetSocket socket = result.result();
-                            // 发送数据
-                            // 构造消息
-                            ProtocolMessage<RpcRequest> protocolMessage = new ProtocolMessage<>();
-                            ProtocolMessage.Header header = new ProtocolMessage.Header();
-                            header.setMagic(ProtocolConstant.PROTOCOL_MAGIC);
-                            header.setVersion(ProtocolConstant.PROTOCOL_VERSION);
-                            header.setSerializer((byte) ProtocolMessageSerializerEnum.getEnumByValue(RpcApplication.getRpcConfig().getSerializer()).getKey());
-                            header.setType((byte) ProtocolMessageTypeEnum.REQUEST.getKey());
-                            header.setRequestId(IdUtil.getSnowflakeNextId());
-                            protocolMessage.setHeader(header);
-                            protocolMessage.setBody(rpcRequest);
-                            // 编码请求
-                            try {
-                                Buffer encodeBuffer = ProtocolMessageEncoder.encode(protocolMessage);
-                                socket.write(encodeBuffer);
-                            } catch (IOException e) {
-                                throw new RuntimeException("协议消息编码错误");
-                            }
-
-                            // 接收响应
-                            socket.handler(buffer -> {
-                                try {
-                                    ProtocolMessage<RpcResponse> rpcResponseProtocolMessage = (ProtocolMessage<RpcResponse>) ProtocolMessageDecoder.decode(buffer);
-                                    responseFuture.complete(rpcResponseProtocolMessage.getBody());
-                                } catch (IOException e) {
-                                    throw new RuntimeException("协议消息解码错误");
-                                }
-                            });
-                        } else {
-                            System.err.println("Failed to connect to TCP server");
-                        }
-                    });
-
-            RpcResponse rpcResponse = responseFuture.get();
-            // 记得关闭连接
-            netClient.close();
+            RpcResponse rpcResponse = VertxTcpClient.doRequest(rpcRequest, selectedServiceMetaInfo);
             return rpcResponse.getData();
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            throw new RuntimeException("调用失败");
         }
+    }
 
-        return null;
+    /**
+     * 发送 HTTP 请求
+     *
+     * @param selectedServiceMetaInfo
+     * @param bodyBytes
+     * @return
+     * @throws IOException
+     */
+    private static RpcResponse doHttpRequest(ServiceMetaInfo selectedServiceMetaInfo, byte[] bodyBytes) throws IOException {
+        final Serializer serializer = SerializerFactory.getInstance(RpcApplication.getRpcConfig().getSerializer());
+        // 发送 HTTP 请求
+        try (HttpResponse httpResponse = HttpRequest.post(selectedServiceMetaInfo.getServiceAddress())
+                .body(bodyBytes)
+                .execute()) {
+            byte[] result = httpResponse.bodyBytes();
+            // 反序列化
+            RpcResponse rpcResponse = serializer.deserialize(result, RpcResponse.class);
+            return rpcResponse;
+        }
     }
 }
--- lightning-rpc/src/main/java/com/rose/lrpc/server/tcp/VertxTcpClient.java
+++ lightning-rpc/src/main/java/com/rose/lrpc/server/tcp/VertxTcpClient.java
@@ -1,30 +1,84 @@
 package com.rose.lrpc.server.tcp;
 
+import cn.hutool.core.util.IdUtil;
+import com.rose.lrpc.RpcApplication;
+import com.rose.lrpc.model.RpcRequest;
+import com.rose.lrpc.model.RpcResponse;
+import com.rose.lrpc.model.ServiceMetaInfo;
+import com.rose.lrpc.protocol.*;
 import io.vertx.core.Vertx;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.net.NetClient;
+import io.vertx.core.net.NetSocket;
 
+import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 
 public class VertxTcpClient {
-    public void start(){
+
+    /**
+     * 发送请求
+     *
+     * @param rpcRequest
+     * @param serviceMetaInfo
+     * @return
+     * @throws InterruptedException
+     * @throws ExecutionException
+     */
+    public static RpcResponse doRequest(RpcRequest rpcRequest, ServiceMetaInfo serviceMetaInfo) throws InterruptedException, ExecutionException {
+        // 发送 TCP 请求
         Vertx vertx = Vertx.vertx();
+        NetClient netClient = vertx.createNetClient();
+        CompletableFuture<RpcResponse> responseFuture = new CompletableFuture<>();
+        netClient.connect(serviceMetaInfo.getServicePort(), serviceMetaInfo.getServiceHost(),
+                result -> {
+                    if (!result.succeeded()) {
+                        System.err.println("Failed to connect to TCP server");
+                        return;
+                    }
+                    NetSocket socket = result.result();
+                    // 发送数据
+                    // 构造消息
+                    ProtocolMessage<RpcRequest> protocolMessage = new ProtocolMessage<>();
+                    ProtocolMessage.Header header = new ProtocolMessage.Header();
+                    header.setMagic(ProtocolConstant.PROTOCOL_MAGIC);
+                    header.setVersion(ProtocolConstant.PROTOCOL_VERSION);
+                    header.setSerializer((byte) ProtocolMessageSerializerEnum.getEnumByValue(RpcApplication.getRpcConfig().getSerializer()).getKey());
+                    header.setType((byte) ProtocolMessageTypeEnum.REQUEST.getKey());
+                    // 生成全局请求 ID
+                    header.setRequestId(IdUtil.getSnowflakeNextId());
+                    protocolMessage.setHeader(header);
+                    protocolMessage.setBody(rpcRequest);
 
-        vertx.createNetClient().connect(8888,"localhost", result->{
-            if(result.succeeded()){
-                System.out.println("Connected to TCP server");
-                io.vertx.core.net.NetSocket socket = result.result();
+                    // 编码请求
+                    try {
+                        Buffer encodeBuffer = ProtocolMessageEncoder.encode(protocolMessage);
+                        socket.write(encodeBuffer);
+                    } catch (IOException e) {
+                        throw new RuntimeException("协议消息编码错误");
+                    }
 
-                socket.write("hello server!");
+                    // 接收响应
+                    TcpBufferHandlerWrapper bufferHandlerWrapper = new TcpBufferHandlerWrapper(
+                            buffer -> {
+                                try {
+                                    ProtocolMessage<RpcResponse> rpcResponseProtocolMessage =
+                                            (ProtocolMessage<RpcResponse>) ProtocolMessageDecoder.decode(buffer);
+                                    responseFuture.complete(rpcResponseProtocolMessage.getBody());
+                                } catch (IOException e) {
+                                    throw new RuntimeException("协议消息解码错误");
+                                }
+                            }
+                    );
+                    socket.handler(bufferHandlerWrapper);
 
-                socket.handler(buffer -> {
-                    System.out.println("Received data from server: " + buffer.toString());
                 });
-            }else {
-                System.out.println("Failed to connect to TCP server: " );
-            }
-        });
-    }
 
-    public static void main(String[] args) {
-        new VertxTcpClient().start();
+        RpcResponse rpcResponse = responseFuture.get();
+        // 记得关闭连接
+        netClient.close();
+        return rpcResponse;
     }
 }
